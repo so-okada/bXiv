ac37092c627dfff3273bbe43a009163a634e74a3
so.okada@gmail.com <so-okada>
2025-02-24 16:04:09 +0900

    update

--- bXiv_format.py
+++ bXiv_format.py
@@ -17,58 +17,55 @@ def format(entries):
 def format_each(orig_entry):
     fixed_length = urls_len + newsub_spacer + margin
     entry = orig_entry.copy()
-    orig_title = entry['title']
+    orig_title = entry["title"]
 
-    authors_title = entry['authors'] + entry['title']
-    current_len =   len(authors_title) + fixed_length
+    authors_title = entry["authors"] + entry["title"]
+    current_len = len(list(authors_title)) + fixed_length
 
     # first,  a title
     if current_len > max_len:
         difference = current_len - max_len
-        current_len_title = len(entry['title'])
+        current_len_title = len(list(entry["title"]))
         lim = max(min_len_title, current_len_title - difference)
-        entry['title'] = simple(entry['title'], lim)
+        entry["title"] = simple(entry["title"], lim)
 
-    authors_title = entry['authors'] + entry['title']
-    current_len = \
-        len(authors_title) + fixed_length
+    authors_title = entry["authors"] + entry["title"]
+    current_len = len(list(authors_title)) + fixed_length
 
     # second, authors
     if current_len > max_len:
         difference = current_len - max_len
-        current_len_authors = \
-            len(entry['authors'])
+        current_len_authors = len(list(entry["authors"]))
         lim = max(min_len_authors, current_len_authors - difference)
-        entry['authors'] = authors(entry['authors'], lim)
+        entry["authors"] = authors(entry["authors"], lim)
 
     # third,  a longer title if the length of authors' names becomes shorter
-    entry['title'] = orig_title
-    authors_title = entry['authors'] + entry['title']
-    current_len = \
-        len(authors_title) + fixed_length
+    entry["title"] = orig_title
+    authors_title = entry["authors"] + entry["title"]
+    current_len = len(list(authors_title)) + fixed_length
 
     if current_len > max_len:
         difference = current_len - max_len
-        current_len_title = len(entry['title'])
+        current_len_title = len(list(entry["title"]))
         lim = max(min_len_title, current_len_title - difference)
-        entry['title'] = simple(entry['title'], lim)
+        entry["title"] = simple(entry["title"], lim)
 
-    entry['separated_abstract'] = \
-        separate_abstract(entry['abstract'], entry['id'],
-                          max_len - abst_tag - margin)
+    entry["separated_abstract"] = separate_abstract(
+        entry["abstract"], entry["id"], max_len - abst_tag - margin
+    )
     return entry
 
 
 # a simple text cut
 def simple(orig, lim):
     orig = orig.strip()
-    wlen = len(orig)
+    wlen = len(list(orig))
     if wlen <= lim:
         return orig
 
     while wlen > lim:
         orig = orig[:-1]
-        wlen = len(orig)
+        wlen = len(list(orig))
 
     return orig[:-3] + "..."
 
@@ -76,52 +73,52 @@ def simple(orig, lim):
 # formating authors' names
 def authors(orig, lim):
     if lim < 1:
-        return ''
-    if len(orig) <= lim:
+        return ""
+    if len(list(orig)) <= lim:
         return orig
 
     collab = collaboration(orig)
     if collab != orig:
-        if len(collab) <= lim:
+        if len(list(collab)) <= lim:
             return collab
         else:
-            return ''
+            return ""
 
     no_paren = noparen(orig)
-    if len(no_paren) <= lim:
+    if len(list(no_paren)) <= lim:
         return no_paren
 
     sr_names = surnames(no_paren)
-    if len(sr_names) <= lim:
+    if len(list(sr_names)) <= lim:
         return sr_names
 
     et_al = etal(no_paren)
-    if len(et_al) <= lim:
+    if len(list(et_al)) <= lim:
         return et_al
 
-    return ''
+    return ""
 
 
 def collaboration(orig):
-    collab = re.match(r'^[^:]+collaboration:', orig, re.IGNORECASE)
+    collab = re.match(r"^[^:]+collaboration:", orig, re.IGNORECASE)
     if collab:
-        return re.sub(':$', '', collab.group())
+        return re.sub(":$", "", collab.group())
     else:
         return orig
 
 
 def noparen(test_str):
-    ret = ''
+    ret = ""
     skip = 0
     for i in test_str:
-        if i == '(':
+        if i == "(":
             skip += 1
-        elif i == ')':
+        elif i == ")":
             skip -= 1
         elif skip == 0:
             ret += i
-    ret = re.sub('[ ]+,', ',', ret)
-    ret = re.sub('[ ]+$', '', ret)
+    ret = re.sub("[ ]+,", ",", ret)
+    ret = re.sub("[ ]+$", "", ret)
     return ret
 
 
@@ -129,48 +126,59 @@ def noparen(test_str):
 
 
 def surnames(orig):
-    names = orig.split(',')
+    names = orig.split(",")
     sr_names = [HumanName(one).last for one in names]
-    separator = ', '
+    separator = ", "
     return separator.join(sr_names)
 
 
 def etal(orig):
-    first_author = re.search(r'[\w|.| ]+,', orig)
+    first_author = re.search(r"[\w|.| ]+,", orig)
     return first_author.group() + " et al."
 
 
 # separate an abstract with a counter and url tag
 def separate_abstract(orig, id, lim):
     sep_abstract = separate(orig, lim)
-    num = len(sep_abstract)
+    num = len(list(sep_abstract))
     result = []
     for i, each in enumerate(sep_abstract):
-        ptext = each + \
-            " [" + str(i+1) + "/" + str(num) + \
-            " of https://arxiv.org/abs/"+id+"v1]"
+        ptext = (
+            each
+            + " ["
+            + str(i + 1)
+            + "/"
+            + str(num)
+            + " of https://arxiv.org/abs/"
+            + id
+            + "v1]"
+        )
         result.append(ptext)
     return result
+
 
 # separate a text by weighted lengths <=lim
 def separate(orig, lim):
     sep_text = []
     orig = orig.strip()
-    
+
     # no inf loop
-    if any(
-            len(t) > lim
-            for t in orig.split(' ')):
-        print('\n**cannot separate** \
-        \nmax weighted length:  ' + str(lim) + ' \ninput:  ' + orig)
+    if any(len(list(t)) > lim for t in orig.split(" ")):
+        print(
+            "\n**cannot separate** \
+        \nmax weighted length:  "
+            + str(lim)
+            + " \ninput:  "
+            + orig
+        )
         return sep_text
 
     while orig:
         partial_text = orig
-        wlen = len(partial_text)
+        wlen = len(list(partial_text))
         while wlen > lim:
-            partial_text = partial_text.rsplit(' ', 1)[0]
-            wlen = len(partial_text)
+            partial_text = partial_text.rsplit(" ", 1)[0]
+            wlen = len(list(partial_text))
         sep_text.append(partial_text.strip())
-        orig = orig[len(partial_text):].strip()
+        orig = orig[len(list(partial_text)) :].strip()
     return sep_text
